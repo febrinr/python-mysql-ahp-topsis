7a617da709c8a2bd2520365940ad5fa98a94abad
Febri Nur Rohman <nurrohman.febri@gmail.com>
2024-03-30 17:34:46 +0700

    Create and use .env to store database configuration settings

--- python-mysql-ahp-topsis/connection.py
+++ python-mysql-ahp-topsis/connection.py
@@ -1,9 +1,15 @@
 import mysql.connector
+import os
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 
 db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="",
-    database="ahp_topsis"
+    host=os.getenv("MYSQL_HOST"),
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    database=os.getenv("MYSQL_DATABASE")
 )
